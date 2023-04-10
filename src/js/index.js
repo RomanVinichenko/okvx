c684e79f0470593080f0aa6f73ac3cccf717856f
r.vinichenko <r.vinichenko@disoft.dev>
2023-04-11 01:21:14 +0300

    3

--- src/js/index.js
+++ src/js/index.js
@@ -6,58 +6,34 @@ const scrollChange = 1
 
 const add_class_on_scroll = () => header.classList.add("header--active")
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   scrollpos = window.scrollY;
-  if (scrollpos >= scrollChange) { add_class_on_scroll() } else {
+  if (scrollpos >= scrollChange) {
+    add_class_on_scroll()
+  } else {
     header.classList.remove("header--active")
   }
 })
 
-
-// let tabChange = function(){
-//   let tabs = $('.nav-pills > li');
-//   let active = tabs.filter('.active');
-//   let next = active.next('li').length? active.next('li').find('a') : tabs.filter(':first-child').find('a');
-//   // Bootsrap tab show, para ativar a tab
-//   next.tab('show')
-// }
-// // Tab Cycle function
-// let tabCycle = setInterval(tabChange, 3000)
-// // Tab click event handler
-// $(function(){
-//   $('.nav-tabs a').click(function(e) {
-//     e.preventDefault();
-//     // Parar o loop
-//     clearInterval(tabCycle);
-//     // mosta o tab clicado, default bootstrap
-//     $(this).tab('show')
-//     // Inicia o ciclo outra vez
-//     setTimeout(function(){
-//       tabCycle = setInterval(tabChange, 1000)//quando recomeça assume este timing
-//     }, 1000);
-//   });
-// });
-
-// Tab-Pane change function
 function tabChange() {
-  var tabs = $('.nav-tabs > li');
-  var active = tabs.filter('.active');
-  var next = active.next('li').length? active.next('li').find('a') : tabs.filter(':first-child').find('a');
+  let tabs = $('.nav-tabs > li');
+  let active = tabs.filter('.active');
+  let next = active.next('li').length ? active.next('li').find('a') : tabs.filter(':first-child').find('a');
   next.tab('show');
 }
 
-$('.tab-pane').hover(function() {
+$('.tab-pane').hover(function () {
   clearInterval(tabCycle);
-}, function() {
+}, function () {
   tabCycle = setInterval(tabChange, 5000);
 });
 
 // Tab Cycle function
-var tabCycle = setInterval(tabChange, 5000)
+let tabCycle = setInterval(tabChange, 5000)
 
 // Tab click event handler
-$(function(){
-  $('.nav-tabs a').click(function(e) {
+$(function () {
+  $('.nav-tabs a').click(function (e) {
     e.preventDefault();
     clearInterval(tabCycle);
     $(this).tab('show')
